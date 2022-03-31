453d92791b538009f81a9f457a9c662d4520722e
Faroukakindele <torchbeareroau@gmail.com>
2022-03-31 01:09:30 +0100

    file

---
+++ index.js
@@ -0,0 +1,9 @@
+// const contact = document.getElementById("Submit")
+// contact.addEventListener("click" , (e)=>{
+//     e.preventDefault
+//     const inputELements = document.getElementsByClassName("newsbmt")
+//     const mailBody = `mailto:faroukakindele0@gmail.com?Name=${inputELements[0].value}&Email=${inputELements[1].value}&subject=${inputELements[3].value}&Body=${inputELements[4].value}`
+//     console.log(inputELements)
+//     console.log(mailBody)
+//     window.location.href=mailBody
+// })
